13144dd65ce5929867fb69cac01f11a137dc161d
Максим Довгаль <dms@itgro.ru>
2020-01-27 16:03:41 +0500

    initial commit

---
+++ example/index.js
@@ -0,0 +1,15 @@
+/* @flow */
+
+import React from 'react';
+import {AppRegistry} from 'react-native';
+
+import {name as appName} from './app.json';
+import ViewPagerExample from './ViewPagerExample';
+
+class ExampleApp extends React.Component<{}> {
+  render() {
+    return <ViewPagerExample />;
+  }
+}
+
+AppRegistry.registerComponent(appName, () => ExampleApp);
